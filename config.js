003a471462baab973153cca2cb667dd5cdcf58dd
chicomint <109532636+chicomint@users.noreply.github.com>
2026-07-18 17:01:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const DEFAULTS = {
     title: 'ChikoChan',
     description: 'Welcome to ChikoChan, (off topic) Talk about any! ,no nsfw',
     anonymousName: 'Anonymous',
-    announcement: ''
+    announcement: 'Chikochan are update today!, nothing much, check some around!'
   },
   limits: {
     maxFileBytes: 5 * 1024 * 1024,
